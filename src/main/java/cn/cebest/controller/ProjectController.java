71a369f1e69e59707ce88245e5fc6278c47a1734
maming <maming@300.cn>
2018-09-11 16:14:41 +0800

    项目基本信息回显

--- src/main/java/cn/cebest/controller/ProjectController.java
+++ src/main/java/cn/cebest/controller/ProjectController.java
@@ -2,12 +2,12 @@ package cn.cebest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import cn.cebest.entity.ProjectEntity;
 import cn.cebest.framework.util.Result;
 import cn.cebest.service.ProjectService;
@@ -19,11 +19,11 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 
-	@PostMapping("selectById")
-	@ResponseBody
-	public ProjectEntity selectById(@RequestParam Long id){
+	@GetMapping("selectById")
+	public String selectById(@RequestParam Long id, ModelMap model){
 		ProjectEntity project = projectService.selectById(id);
-		return project;
+		model.put("project", project);
+		return "/project";
 	}
 	
 	@PostMapping("/insertOrUpdate")
