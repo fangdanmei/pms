414767251222d597a1b46abcf684919dedba01fc
maming <maming@300.cn>
2018-09-29 10:36:59 +0800

    添加项目风险实体类

---
+++ src/main/java/cn/cebest/entity/ProjectRisk.java
@@ -0,0 +1,44 @@
+package cn.cebest.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@TableName("PROJECT_RISK")
+public class ProjectRisk extends Model<ProjectRisk>{
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	private Integer projectId;
+	
+	private String describe;
+	
+	private String level;
+	
+	private String probability;
+	
+	private String state;
+	
+	private Date trackTime;
+	
+	private String measures;
+	
+	private String personLiable;
+	
+	private Date createTime;
+	
+	private Date updateTime;
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+	
+}
