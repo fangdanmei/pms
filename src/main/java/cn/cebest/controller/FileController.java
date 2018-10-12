c59396bbd0c03b68d9b1a1d2a00bfb488c3e2ffd
maming <maming@300.cn>
2018-10-12 14:32:18 +0800

    添加附件下载相关

--- src/main/java/cn/cebest/controller/FileController.java
+++ src/main/java/cn/cebest/controller/FileController.java
@@ -5,25 +5,31 @@ import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import cn.cebest.util.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 文件上传
+ * 
  * @author maming
  * @date 2018年9月6日
  */
 
+@Slf4j
 @RestController
-public class FileUploadController {
+public class FileController {
 
 	@Value("${file.upload.path}")
 	private String path;
 
-	
 	@PostMapping("/upload")
 	public Map<String, Object> upload(@RequestParam(value = "file", required = true) MultipartFile file)
 			throws Exception {
@@ -49,6 +55,22 @@ public class FileUploadController {
 		data.put("fileName", fileName);
 		result.put("data", data);
 		return result;
+	}
+
+	
+	
+	@GetMapping(value = "/download/{fileName}/{originName}")
+	public void download(@PathVariable("fileName") String fileName, @PathVariable("originName") String originName,
+			HttpServletResponse response) {
+		String[] strs = fileName.split("\\.");
+		originName += "." + strs[strs.length - 1];
+		String filePath = path + File.separator + fileName;
+		try {
+			// 下载文件
+			FileUtil.fileDownload(response, filePath, originName);
+		} catch (Exception e) {
+			log.error("文件下载异常", e);
+		}
 	}
 
 }
--- src/main/java/cn/cebest/util/FileUtil.java
+++ src/main/java/cn/cebest/util/FileUtil.java
@@ -1,10 +1,19 @@
 package cn.cebest.util;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.slf4j.Slf4j;
 
@@ -44,5 +53,74 @@ public class FileUtil {
 		}
 
 		return result;
+	}
+	
+	
+	/**
+	 * @param response 
+	 * @param filePath		//文件完整路径(包括文件名和扩展名)
+	 * @param fileName		//下载后看到的文件名
+	 * @return  文件名
+	 * @throws IOException 
+	 */
+	public static void fileDownload(final HttpServletResponse response, String filePath, String fileName) throws IOException{  
+		   
+		byte[] data = toByteArray(filePath);  
+	    fileName = URLEncoder.encode(fileName.substring(0, fileName.lastIndexOf(".")), "UTF-8") + fileName.substring(fileName.lastIndexOf("."));  
+	    response.reset();  
+	    response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");  
+	    response.addHeader("Content-Length", "" + data.length);  
+	    response.setContentType("application/octet-stream;charset=UTF-8");  
+	    OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());  
+	    outputStream.write(data);  
+	    outputStream.flush();  
+	    outputStream.close();
+	    response.flushBuffer();
+	    
+	} 
+	
+	
+	/**
+	 * 读取到字节数组2
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static byte[] toByteArray(String filePath) throws IOException {
+		File f = new File(filePath);
+		if (!f.exists()) {
+			throw new FileNotFoundException(filePath);
+		}
+		FileChannel channel = null;
+		FileInputStream fs = null;
+		ByteBuffer byteBuffer = null;
+		try {
+			fs = new FileInputStream(f);
+			channel = fs.getChannel();
+			byteBuffer = ByteBuffer.allocate((int) channel.size());
+			while ((channel.read(byteBuffer)) > 0) {
+			}
+			
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+			throw e;
+		} finally {
+			try {
+				if(channel != null){
+					channel.close();
+				}
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+			try {
+				if(fs != null){
+					fs.close();
+				}
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+		return byteBuffer.array();
 	}
 }
