3726b8939bc07888fd0629b334d03b5e54e51d88
maming <maming@300.cn>
2018-10-23 17:00:20 +0800

    删除无效文件

--- src/main/java/cn/cebest/controller/ProjectController.java
+++ src/main/java/cn/cebest/controller/ProjectController.java
@@ -2,8 +2,6 @@ package cn.cebest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
